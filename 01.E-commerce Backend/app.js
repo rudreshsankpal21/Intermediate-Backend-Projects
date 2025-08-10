4715f07f8ff8777c7b22001213786b5ad06ea61a
rudresh21 <rudreshsankpal21@gmail.com>
2025-08-10 19:54:38 +0530

    feat : add category model & routes

--- 01.E-commerce Backend/app.js
+++ 01.E-commerce Backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const authRouter = require("./routes/authRoutes");
+const authMiddleware = require("./middlewares/authMiddleware");
+const categoryRouter = require("./routes/categoryRoutes");
 const app = express();
 
 // middlewares
@@ -8,5 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use("/api/auth", authRouter);
+app.use("/api/categories", authMiddleware, categoryRouter);
 
 module.exports = app;
---
+++ 01.E-commerce Backend/routes/categoryRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const isAdmin = require("../middlewares/isAdmin");
+
+const categoryRouter = express.Router();
+
+// create Category
+categoryRouter.post("/", isAdmin, createCategory);
+
+// get all categories
+categoryRouter.get("/", getAllCategories);
+
+// get category by id
+categoryRouter.get("/:id", getCategoryById);
+
+// update category by id
+categoryRouter.put("/:id", isAdmin, updateCategoryById);
+
+// delete category by id
+categoryRouter.delete("/:id", isAdmin, deleteCategoryById);
+
+module.exports = categoryRouter;
