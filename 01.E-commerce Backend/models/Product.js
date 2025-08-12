b6a1abc20b367bd6e1e6f3b6a79f5babea2ec998
rudresh21 <rudreshsankpal21@gmail.com>
2025-08-12 22:43:21 +0530

    feat : add product Model

---
+++ 01.E-commerce Backend/models/Product.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    images: {
+      type: Array,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const product = mongoose.model("Product", productSchema);
+export default product;
